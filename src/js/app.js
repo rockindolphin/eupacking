9460ad7fb80d5d70dc3b2fdd6124ace491da7271
Unknown <rockindolphing@gmail.com>
2018-05-30 17:03:54 +0300

    mobile view

--- src/js/app.js
+++ src/js/app.js
@@ -80,6 +80,24 @@
 				navigation: {
 					nextEl: '.swiper-button-next',
 					prevEl: '.swiper-button-prev',
+				},
+				breakpoints: {
+					576: {
+						slidesPerView: 1,
+						spaceBetween: 0,
+					},
+					768: {
+						slidesPerView: 2,
+						spaceBetween: 30,
+					},
+					992: {
+						slidesPerView: 3,
+						spaceBetween: 30,
+					},
+					1200: {
+						slidesPerView: 4,
+						spaceBetween: 30,
+					}
 				}
 			});
 		});
